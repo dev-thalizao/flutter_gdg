c70a7407f79b546b7332abcb0a75550ef11d54c3
Thales Frigo <thales.frigo@techfitapps.com>
2018-07-02 13:08:21 -0300

    Create file placeholders

---
+++ lib/list/team_card.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TeamCard extends StatefulWidget {
+
+  @override
+  State createState() => TeamCardState();
+}
+
+class TeamCardState extends State<TeamCard> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+
+//  void navigateToDetail(){}
+}
---
+++ lib/list/teams_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TeamsPage extends StatefulWidget {
+
+  @override
+  State createState() => TeamsPageState();
+}
+
+class TeamsPageState extends State<TeamsPage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
